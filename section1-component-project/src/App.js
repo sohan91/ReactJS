dc49794f695e0263fa73450a5338aecf5419eb63
sohan <sohanprasad2003@gmail.com>
2026-01-27 09:35:19 +0530

    load image,create custom components

--- section1-component-project/src/App.js
+++ section1-component-project/src/App.js
@@ -1,11 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { SUMMARY } from './data.js';
+
+function Header() {
+  return (
+    <div className="head-section">
+      <h1>MOVIE DESCRIPTION</h1>
+    </div>
+  );
+}
+
+function MovieContainer() {
+  return (
+    <div className="middle-container">
+      <ul>
+        {
+        SUMMARY.map((values, index) => (
+          <MovieCards
+            title={values.title}
+            images={values.img}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function MovieCards({ title, images }) {
+  return (
+    <li className="card-container">
+      <span>{title}</span>
+      <img src={images} alt={title} />
+    </li>
+  );
+}
+
+function DescriptionContainer() {
+  return <footer className="last-container"></footer>;
+}
 
 function App() {
   return (
-   
-       <h1>Hello Word!! </h1>
-    
+    <div className="container">
+      <Header />
+      <MovieContainer />
+      <DescriptionContainer />
+    </div>
   );
 }
 
---
+++ section1-component-project/src/data.js
@@ -0,0 +1,21 @@
+import bahuImg from './images/bahubali.jpg';
+import intersteller from './images/interstellar.jpg';
+import uri from './images/URI.jpg';
+
+export  let SUMMARY = [
+    {
+        title:"URI",
+        img:uri,
+        description:"UIR info"
+    },
+    {
+        title:"Bahubali",
+        img:bahuImg,
+        description:"BAHUBALI info"
+    },
+    {
+        title:"Instersteller",
+        img:intersteller,
+        description:"INTERSTELLAR INFO"
+    }
+]
